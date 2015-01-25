fc169fa0b9010888d90fb65a0ed73b74b9fb7c5a
Mark Myers <marcusmyers@gmail.com>
2015-01-25 16:04:22 -0500

    Added apt-get command

--- puppet/manifests/db.pp
+++ puppet/manifests/db.pp
@@ -1,6 +1,10 @@
 node db {
   class { '::mysql::server':
     root_password   => 'str0ngp4ssW0r6!',
-    require => Exec['apt-get update'],
+    require => Exec['AptUpdate'],
+  }
+
+  exec { 'AptUpate':
+    command => 'apt-get update',
   }
 }
