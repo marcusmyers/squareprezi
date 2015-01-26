1cf44c9cc4d2f2752eb76b696d490f0c61f004f4
Mark Myers <marcusmyers@gmail.com>
2015-01-26 11:54:06 -0500

    Added before to apt class

--- puppet/manifests/rails.pp
+++ puppet/manifests/rails.pp
@@ -1,10 +1,11 @@
 node rails {
   class { 'apt':
     always_apt_update => true,
-  }~>
-  class { '::mysql::client':  }~>
-  class { 'rbenv':  }
+    before            => Class['rbenv'],
+  }
 
+  class { 'rbenv':  }
+  class { '::mysql::client':  }
   rbenv::plugin { 'sstephenson/ruby-build': }
   rbenv::build { '2.0.0-p576': global => true }
 }
