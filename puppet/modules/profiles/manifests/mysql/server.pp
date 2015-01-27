4bd27a7bdd0ccda65763a436f1897caf3a0e23ac
Mark Myers <marcusmyers@gmail.com>
2015-01-26 22:02:40 -0500

    fixed #8 - Allow the rails server to access mysql server

--- puppet/modules/profiles/manifests/mysql/server.pp
+++ puppet/modules/profiles/manifests/mysql/server.pp
@@ -5,6 +5,7 @@ class profiles::mysql::server ( $root_password='school', $bindings=[] ) inherits
     root_password => "${root_password}",
   }
 
+  # Allow the rails box access to mysql
   ufw::allow { 'allow-mysql-from-rails':
     port => 3306,
     from => "10.20.1.2",
