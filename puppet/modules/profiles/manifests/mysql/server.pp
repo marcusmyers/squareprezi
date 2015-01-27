25e3a3862e620cd62b22abbc72e3c01ecef09ef8
Mark Myers <marcusmyers@gmail.com>
2015-01-26 21:50:55 -0500

    Added 3306 unblocked for rails server

--- puppet/modules/profiles/manifests/mysql/server.pp
+++ puppet/modules/profiles/manifests/mysql/server.pp
@@ -1,5 +1,12 @@
 class profiles::mysql::server ( $root_password='school', $bindings=[] ) inherits profiles {
+  include ufw
+
   class { '::mysql::server':
     root_password => "${root_password}",
+  }
+
+  ufw::allow { 'allow-mysql-from-rails':
+    port => 3306,
+    from => "10.20.1.2",
   }
 }
