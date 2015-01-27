d5a71abac94dc2601e710f49edf1cd9e52c4f13b
Mark Myers <marcusmyers@gmail.com>
2015-01-27 08:40:14 -0500

    Fixed boot time for ruby installation using brightbox PPA

--- puppet/modules/profiles/manifests/aptruby.pp
+++ puppet/modules/profiles/manifests/aptruby.pp
@@ -1,14 +1,14 @@
-class profiles::aptruby ($version='2.0') {
-  apt::ppa { 'ppa:brightbox/ruby-ng': }
-  
+class profiles::aptruby ($version='2.0') inherits profiles {
   $ruby = "ruby${version}"
   $rubyDev = "${ruby}-dev"
-
+  
+  class { 'apt':  }
+  apt::ppa { 'ppa:brightbox/ruby-ng':  }->
   package { "${ruby}":
     ensure => installed,
   }
-
   package { "${rubyDev}": 
-    ensure => installed,
+    ensure  => installed,
+    require => Package["${ruby}"],
   }
 }
