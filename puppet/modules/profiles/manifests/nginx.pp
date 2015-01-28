0f346d2941f3018945670d74a9a8e1411e77e28c
Mark Myers <marcusmyers@gmail.com>
2015-01-27 22:10:23 -0500

    Get everything setup to run rails app

--- puppet/modules/profiles/manifests/nginx.pp
+++ puppet/modules/profiles/manifests/nginx.pp
@@ -1,37 +1,5 @@
 class profiles::nginx ($docroot='/home/vagrant/code/public') {
 
-#  package { 'apt-transport-https':
-#    ensure => installed,
-#    before => Package['ca-certificates'],
-#  }
-
-#  package { 'ca-certificates':
-#    ensure => installed,
-#    before => Apt_key['passenger']
-#  }
-
-#  apt_key { 'passenger':
-#    ensure => 'present',
-#    id     => '561F9B9CAC40B2F7',
-#    server => 'hkp://keyserver.ubuntu.com:80',
-#    notify => Apt::Source['passenger'],
-#  }
-
-#  apt::source { 'passenger':
-#    location => 'https://oss-binaries.phusionpassenger.com/apt/passenger',
-#    repos    => 'main',
-#    release  => 'trusty',
-#    notify   => Package['passenger'],
-#  }
-
-#  package { 'passenger':
-#    ensure => installed,
-#  }
-
-#  package { 'nginx-extras': 
-#    ensure => installed,
-#  }
-
   class { '::nginx':
     package_source  => 'passenger',
     package_name    => 'nginx-extras',
@@ -41,9 +9,10 @@ class profiles::nginx ($docroot='/home/vagrant/code/public') {
   }
 
   nginx::resource::vhost { 'localhost':
-    www_root  => "${docroot}",
-    autoindex => 'off',
-    vhost_cfg_append => {
+    www_root             => "${docroot}",
+    autoindex            => 'off',
+    use_default_location => false,
+    vhost_cfg_append     => {
       'passenger_enabled' => 'on',
       'passenger_ruby'    => '/usr/bin/ruby',
       'rails_env'         => 'development',
