d0044c9d50e8533274f35becaeb90f19484ec46c
Mark Myers <marcusmyers@gmail.com>
2015-01-28 10:15:45 -0500

    Added default database data to mysql::server class

--- puppet/modules/profiles/manifests/mysql/server.pp
+++ puppet/modules/profiles/manifests/mysql/server.pp
@@ -1,13 +1,20 @@
-class profiles::mysql::server ( $root_password='school', $bindings=[] ) inherits profiles {
+class profiles::mysql::server ( $root_password='secret', $db='samplerails',$user='railsuser',$password='new*data', $host='10.20.1.2' ) inherits profiles {
   include ufw
 
   class { '::mysql::server':
     root_password => "${root_password}",
   }
 
+  mysql::db { "${db}":
+    user     => "${user}",
+    password => "${password}",
+    host     => "${host}",
+    grant    => ['ALL'],
+  }
+
   # Allow the rails box access to mysql
   ufw::allow { 'allow-mysql-from-rails':
     port => 3306,
-    from => "10.20.1.2",
+    from => "${host}",
   }
 }
