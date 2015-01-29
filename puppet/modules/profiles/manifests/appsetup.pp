8f265141d9e0a39b43e1151fe6126f46550f9a1a
Mark Myers <marcusmyers@gmail.com>
2015-01-29 11:51:33 -0500

    Pull repo from github on boot

--- puppet/modules/profiles/manifests/appsetup.pp
+++ puppet/modules/profiles/manifests/appsetup.pp
@@ -4,11 +4,18 @@ class profiles::appsetup {
     require => File['/usr/bin/gem'],
   }
 
+  vcsrepo { '/home/vagrant/code':
+    ensure   => present,
+    provider => git,
+    source   => "https://github.com/marcusmyers/sample_app_rails_4.git"   
+  }
+
   exec { 'bundle':
     command => '/usr/local/bin/bundle install --without production',
     cwd     => '/home/vagrant/code',
     unless  => '/usr/bin/gem list | grep -c "activerecord (4.0.8)"',
-    require => Exec['/usr/bin/gem install bundler'],
+#    require => Exec['/usr/bin/gem install bundler'],
+    require => Vcsrepo['/home/vagrant/code'],
   }
 
 }
